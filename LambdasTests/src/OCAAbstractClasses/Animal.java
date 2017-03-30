a23535bf8af8131c2fe363d0fe4f8406b148c187
Evandro Machado <evandrolmass@gmail.com>
2017-03-29 21:40:08 -0300

    Learning how to create abstract classes.

---
+++ LambdasTests/src/OCAAbstractClasses/Animal.java
@@ -0,0 +1,11 @@
+package OCAAbstractClasses;
+
+public abstract class Animal {
+	protected int age;
+	
+	public void eat(){
+		System.out.println("Animal is eating");
+	}
+	
+	public abstract String getName();
+}
---
+++ LambdasTests/src/OCAAbstractClasses/Swan.java
@@ -0,0 +1,9 @@
+package OCAAbstractClasses;
+
+public class Swan extends Animal {
+
+	public String getName() {
+		return "Swan";
+	}
+
+}
