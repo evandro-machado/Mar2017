c9a49cd60790a14c9067747f1778d4b221d3b033
evandro-machado <evandrolmass@gmail.com>
2017-04-12 13:38:25 -0300

    Learning how to implement default methods in interfaces.

---
+++ LambdasTests/src/OCAInterfaceDefaultMethods/SharkFamily.java
@@ -0,0 +1,8 @@
+package OCAInterfaceDefaultMethods;
+
+public interface SharkFamily extends HasFins{
+	public default int getNumberOfFins(){
+		return 8;
+	}
+	public double getLongestFinLength();
+}
